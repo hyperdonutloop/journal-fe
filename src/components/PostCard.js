18939008d31fdeb83f11fe5eba3955fc8c536b63
donutwizard666 <thebeardedwarrior@icloud.com>
2020-03-07 09:45:00 -0800

    used moment for date and was able to split date into three parts for entries

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -2,71 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Card, Container } from 'semantic-ui-react';
 import styled from 'styled-components';
+import Moment from 'react-moment';
 
-const Wrapper = styled.div`
-	.post-card {
-	.date {
-		font-size: 1.8em;
-		line-height: 1em;
-		margin-bottom: 20px;
-	}
+import { Wrapper } from '../styles/PostCardStyles.js';
 
-	.post-title {
-		font-size: 1.2em;
-		/* font-family: $font-content; */
-	}
-}
-`;
-
-export default function PostCard ({ title, date, id }) {
-  const changedate = date.split('-');
-	const item3 = changedate[2];
-	const item3split = item3.split('T');
-	const month = changedate[1];
-	const day = item3split[0];
-	const year = changedate[0];
-	const dateword = () => {
-		if (month === '01') {
-			return 'January';
-		}
-		else if (month === '02') {
-			return 'February';
-		}
-		else if (month === '03') {
-			return 'March';
-		}
-		else if (month === '04') {
-			return 'April';
-		}
-		else if (month === '05') {
-			return 'May';
-		}
-		else if (month === '06') {
-			return 'June';
-		}
-		else if (month === '07') {
-			return 'July';
-		}
-		else if (month === '08') {
-			return 'August';
-		}
-		else if (month === '09') {
-			return 'September';
-		}
-		else if (month === '10') {
-			return 'October';
-		}
-		else if (month === '11') {
-			return 'November';
-		}
-		else if (month === '12') {
-			return 'December';
-		}
-		else {
-			return 'Not a Month';
-		}
-	};
-  const datedisplay = `${dateword()} ${day}, ${year}`;
+export default function PostCard ({ title, id }) {
+	const date = new Date();
   
   return (
 		<Wrapper>
@@ -76,7 +17,15 @@ export default function PostCard ({ title, date, id }) {
           <Card raised fluid className="card-container">
             <Card.Content>
               <Card.Header>
-                <div className="date">{datedisplay}</div>
+								<div className="weekday">
+									<Moment format="ddd">{date}</Moment>
+								</div>
+                <div className="date">
+									<Moment format="MMMM Do, YYYY">{date}</Moment>
+								</div>
+								<div className="time">
+									<Moment format="h:mm a">{date}</Moment>
+								</div>
               </Card.Header>
               <Card.Meta>
                 <div className="post-title">{title}</div>
---
+++ src/styles/PostCardStyles.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  .post-card {
+	  .date {
+      font-size: 1.8em;
+      line-height: 1em;
+      margin-bottom: 20px;
+    }
+    
+    .weekday {
+      display: flex;
+    }
+
+	.post-title {
+		font-size: 1.2em;
+		/* font-family: $font-content; */
+	}
+}
+`;
